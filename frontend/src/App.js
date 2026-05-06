9398f0593ecfdb2cde1de03a8c7177861c670023
Juhi <juhi@MacBook-Air.local>
2026-05-06 21:29:38 +0530

    Added protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Feed from "./pages/Feed";
@@ -7,15 +7,42 @@ import PostDetail from "./pages/PostDetail";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/create" element={<CreatePost />} />
-        <Route path="/post/:id" element={<PostDetail />} />
+
+        {/* Home */}
+        <Route
+          path="/"
+          element={token ? <Feed /> : <Navigate to="/login" />}
+        />
+
+        {/* Auth */}
+        <Route
+          path="/login"
+          element={!token ? <Login /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/signup"
+          element={!token ? <Signup /> : <Navigate to="/" />}
+        />
+
+        {/* Protected Routes */}
+        <Route
+          path="/create"
+          element={token ? <CreatePost /> : <Navigate to="/login" />}
+        />
+
+        <Route
+          path="/post/:id"
+          element={token ? <PostDetail /> : <Navigate to="/login" />}
+        />
+
       </Routes>
     </BrowserRouter>
   );
